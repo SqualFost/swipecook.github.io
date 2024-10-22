fec3865491f8566890ac405f37e6666487ed21d5
SqualFost <146347981+SqualFost@users.noreply.github.com>
2024-10-22 22:00:26 +0200

    Like Dislike
    Ajout d'une card avec description du plat + boutons like dislike

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Cards from './assets/components/cards/Cards';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className='d-flex justify-content-center align-items-center vh-100'>
+      <Cards/>
+    </div>
+  );
+}
+
+export default App;
